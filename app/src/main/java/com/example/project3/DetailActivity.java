9b3a50b67c988fd4bdf88a3a7e05e87dd1617375
배민이 <minleebae@gmail.com>
2020-04-23 14:34:17 +0900

    전화걸기 추가

--- app/src/main/java/com/example/project3/DetailActivity.java
+++ app/src/main/java/com/example/project3/DetailActivity.java
@@ -2,14 +2,16 @@ package com.example.project3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class DetailActivity extends AppCompatActivity {
 
-    Button btn_play, btn_stop;
+    Button btn_play, btn_stop, btn_call, btn_send;
     MediaPlayer player;
 
     @Override
@@ -19,6 +21,8 @@ public class DetailActivity extends AppCompatActivity {
 
         btn_play = findViewById(R.id.btn_play);
         btn_stop = findViewById(R.id.btn_stop);
+        btn_call = findViewById(R.id.btn_call);
+        btn_send = findViewById(R.id.btn_send);
 
         btn_play.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +39,15 @@ public class DetailActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 player.stop();
+            }
+        });
+
+
+        btn_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(("tel:010-1111-2222")));
+                startActivity(intent);
             }
         });
     }
