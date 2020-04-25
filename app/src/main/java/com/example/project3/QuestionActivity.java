0d290260dfe55ea5d3a864899d57ea4c57d3d3e2
배민이 <minleebae@gmail.com>
2020-04-25 15:46:10 +0900

    배민이 - QuestionActivity

--- app/src/main/java/com/example/project3/QuestionActivity.java
+++ app/src/main/java/com/example/project3/QuestionActivity.java
@@ -2,13 +2,27 @@ package com.example.project3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuestionActivity extends AppCompatActivity {
+    Button btn_cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        btn_cancel = findViewById(R.id.btn_cancel);
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(QuestionActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
