168bef8fbe3acc61e4c2b659393ca95b3ae3c791
배민이 <minleebae@gmail.com>
2020-05-06 18:10:25 +0900

    성순 - 알람 삭제

--- app/src/main/java/com/example/project3/DetailActivity.java
+++ app/src/main/java/com/example/project3/DetailActivity.java
@@ -51,7 +51,11 @@ public class DetailActivity extends AppCompatActivity {
 
         final String num = intent.getExtras().getString("num"); // 클릭한 알람 번호 가져오기
         final String id = intent.getExtras().getString("loginid");
-
+        final String pw = intent.getExtras().getString("pw");
+        final String addr = intent.getExtras().getString("addr");
+        final String phone = intent.getExtras().getString("phone");
+        final String name = intent.getExtras().getString("name");
+        final String sex = intent.getExtras().getString("sex");
         btn_play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -81,7 +85,7 @@ public class DetailActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 try {
-                    String result  = new DetailActivity.CustomTask_delete().execute(alarm_num,"delete").get();
+                    String result  = new DetailActivity.CustomTask().execute(alarm_num,"delete").get();
                     Log.d("받아온 값", result);
 
                     if (result.contains("0")) {
@@ -91,6 +95,12 @@ public class DetailActivity extends AppCompatActivity {
                     } else {
                         Toast.makeText(DetailActivity.this, "삭제성공", Toast.LENGTH_SHORT).show();
                         Intent intent = new Intent(DetailActivity.this, MainActivity.class);
+                        intent.putExtra("loginid",id);
+                        intent.putExtra("pw",pw);
+                        intent.putExtra("addr",addr);
+                        intent.putExtra("phone",phone);
+                        intent.putExtra("name",name);
+                        intent.putExtra("sex",sex);
                         startActivity(intent);
                         finish();
                     }
@@ -125,48 +135,6 @@ public class DetailActivity extends AppCompatActivity {
     }
     // 알람 번호 서버에 전송
     class CustomTask extends AsyncTask<String, Void, String> {
-        String sendMsg, receiveMsg;
-        @Override
-        // doInBackground의 매개값이 문자열 배열인데요. 보낼 값이 여러개일 경우를 위해 배열로 합니다.
-        protected String doInBackground(String... strings) {
-            try {
-                String str;
-                URL url = new URL("http://192.168.56.1:8081/WellDeep/Alarm_get_android.jsp"); //보낼 jsp 주소를 ""안에 작성합니다.
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                conn.setRequestMethod("POST");//데이터를 POST 방식으로 전송합니다.
-                OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
-                sendMsg = "num="+strings[0]+"&type="+strings[1];//보낼 정보인데요. GET방식으로 작성합니다. ex) "id=rain483&pwd=1234";
-                Log.e("send",sendMsg);
-                //회원가입처럼 보낼 데이터가 여러 개일 경우 &로 구분하여 작성합니다.
-                osw.write(sendMsg);//OutputStreamWriter에 담아 전송합니다.
-                osw.flush();
-                //jsp와 통신이 정상적으로 되었을 때 할 코드들입니다.
-                if(conn.getResponseCode() == conn.HTTP_OK) {
-                    InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
-                    BufferedReader reader = new BufferedReader(tmp);
-                    StringBuffer buffer = new StringBuffer();
-                    //jsp에서 보낸 값을 받겠죠?
-                    while ((str = reader.readLine()) != null) {
-                        buffer.append(str);
-                    }
-                    receiveMsg = buffer.toString();
-
-                } else {
-                    Log.i("통신 결과", conn.getResponseCode()+"에러");
-                    // 통신이 실패했을 때 실패한 이유를 알기 위해 로그를 찍습니다.
-                }
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            //jsp로부터 받은 리턴 값입니다.
-            return receiveMsg;
-        }
-    }
-    class CustomTask_delete extends AsyncTask<String, Void, String> {
         String sendMsg, receiveMsg;
         @Override
         // doInBackground의 매개값이 문자열 배열인데요. 보낼 값이 여러개일 경우를 위해 배열로 합니다.
